cd98e2703831d16a52090a4b2f6bfa4e39fa4231
Polytrope <alexandre78ferrera@gmail.com>
2016-12-02 09:07:03 +0100

    options xml

--- projet_prod_cons/src/projet_prod_cons/TestProdCons.java
+++ projet_prod_cons/src/projet_prod_cons/TestProdCons.java
@@ -6,87 +6,117 @@ import jus.poc.prodcons.*;
 
 public class TestProdCons  extends Simulateur {
 	private Observateur obs;
-	private int taille_tampon=3;
-	int nb_conso=2;
-	int nb_prod=2;
 	Tampon tampon;
 	ArrayList <Producteur> p;
 	ArrayList <Consommateur> c;
 	
-	public TestProdCons(Observateur observateur) {
-		super(observateur);
-		this.obs=observateur;
-		
-		
-		// TODO Auto-generated method stub
-		//corps du programme principal
-		//TODO recuperer l nombre de producteur 
-		
-		//TODO recuperer nombre conso
-		
-		
-	//TODO recuperer taille du tampon 
-		
-		tampon = new ProdCons(taille_tampon);
-		
-		
-		Acteur prod;
-		p= new ArrayList<Producteur>();// posse possiblement probleme comme initialisation
-		int moyenneTempsDeTraitement= 10;//TODO a changer
-		int deviationTempsDeTraitement =5;//TODO a changer
-		for (int i=0;i<=nb_prod;i++){
-		
-			try {
-				prod=new Producteur(Producteur.Prod(), obs, moyenneTempsDeTraitement, deviationTempsDeTraitement, tampon);
-				p.add((Producteur)prod); 
-			} catch (ControlException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-		
-			}
-		
-			
-		
-		}
-		
-		c= new  ArrayList<Consommateur>();// posse possiblement probleme comme initialisation
-		moyenneTempsDeTraitement= 10;//TODO a changer
-		deviationTempsDeTraitement =5;//TODO a changer
-		for (int i=0;i<=nb_prod;i++){
-			try {
-				c.add(new Consommateur(Consommateur.Cons(), obs, moyenneTempsDeTraitement, deviationTempsDeTraitement, tampon));
-			} catch (ControlException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		}
-		
-	}
-
+	//******************** OPTIONS ************
 	
+		protected static int nbProd;
+		protected static int nbCons;
+		protected static int nbBuffer;
+		protected static int tempsMoyenProduction;
+		protected static int deviationTempsMoyenProduction;
+		protected static int tempsMoyenConsommation;
+		protected static int deviationTempsMoyenConsommation;
+		protected static int nombreMoyenDeProduction;
+		protected static int deviationNombreMoyenDeProduction;
+		protected static int nombreMoyenNbExemplaire;
+		protected static int deviationNombreMoyenNbExemplaire;
+
+		
+		public TestProdCons(Observateur observateur, String option) {
+			super(observateur);
+			this.obs=observateur;
+			
+			init(option);
+			
+			tampon = new ProdCons(nbBuffer);
+			
+			
+			Acteur prod;
+			p= new ArrayList<Producteur>();
+			for (int i=0;i<nbProd;i++){
+				System.out.println("je suis la\n");
+				try {
+					prod=new Producteur(Producteur.Prod(), obs, tempsMoyenProduction, deviationTempsMoyenProduction, tampon);
+					p.add((Producteur)prod); 
+				} catch (ControlException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			
+				}
+			
+				
+			
+			}
+			
+			c= new  ArrayList<Consommateur>();
+			for (int i=0;i<=nbCons;i++){
+				try {
+					c.add(new Consommateur(Consommateur.Cons(), obs, tempsMoyenProduction, deviationTempsMoyenConsommation, tampon));
+				} catch (ControlException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			}
+			
+		}
+
 
 	@Override
 	protected void run() throws Exception {
 
-		boolean b= true;
-		for (int i=0;i<nb_prod;i++){
+		for (int i=0;i<nbProd;i++){
 			if (p.get(i)==null) System.out.println("petit probleme\n");
 			else System.out.println(p.get(i).toString());
 			p.get(i).start();
 			//b=b && p.get(i).isAlive();
 		}
-		for (int i=0;i<nb_conso;i++){
+		for (int i=0;i<nbCons;i++){
 			System.out.println(c.get(i).toString());
 			c.get(i).start();
 		}
 	}
 
 	public static void main(String[] args) {
-		new TestProdCons(new Observateur()).start();
+		new TestProdCons(new Observateur(),"option.xml").start();
 	}
 	
 	
 	
+
+	/**
+	* Retreave the parameters of the application.
+	* @param file the final name of the file containing the options.
+	*/
+	protected static void init(String file) {
+	// retreave the parameters of the application
+
+		final class Properties extends java.util.Properties {
+			private static final long serialVersionUID = 1L;
+			public int get(String key){return Integer.parseInt(getProperty(key));}
+			public Properties(String file) {
+				try{
+					loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+				}catch(Exception e){e.printStackTrace();}
+			}
+		}
+		Properties option = new Properties("prodcons/options/"+file);
+		nbProd = option.get("nbProd");
+		System.out.println("nb_prod =" + nbProd);
+		nbCons = option.get("nbCons");
+		nbBuffer = option.get("nbBuffer");
+		tempsMoyenProduction = option.get("tempsMoyenProduction");
+		deviationTempsMoyenProduction = option.get("deviationTempsMoyenProduction");
+		tempsMoyenConsommation = option.get("tempsMoyenConsommation");
+		deviationTempsMoyenConsommation = option.get("deviationTempsMoyenConsommation");
+		nombreMoyenDeProduction = option.get("nombreMoyenDeProduction");
+		deviationNombreMoyenDeProduction = option.get("deviationNombreMoyenDeProduction");
+		nombreMoyenNbExemplaire = option.get("nombreMoyenNbExemplaire");
+		deviationNombreMoyenNbExemplaire = option.get("deviationNombreMoyenNbExemplaire");
+		}
+
 
 
 
